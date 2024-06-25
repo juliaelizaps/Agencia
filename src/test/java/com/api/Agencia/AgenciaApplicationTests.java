89522645d7054f7290534d670cf3976640d7e39b
Julia Eliza <111099871+juliaelizaps@users.noreply.github.com>
2024-06-25 19:29:16 -0300

    api Update

--- src/test/java/com/api/Agencia/AgenciaApplicationTests.java
+++ src/test/java/com/api/Agencia/AgenciaApplicationTests.java
@@ -3,11 +3,11 @@ package com.api.Agencia;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class AgenciaApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
